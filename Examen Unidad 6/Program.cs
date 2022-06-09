9e11cdd72d72e9c5f8563370175ee5dcb3bd57e6
Eduardo Reynoso Rosales <105455890+EddieReynosoR@users.noreply.github.com>
2022-06-09 11:40:33 -0700

    Archivos de mi examen

---
+++ Examen Unidad 6/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_Unidad_6
+{
+    //Reynoso Rosales Eduardo Guadalupe
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            //variable auxiliar y instancia de la clase Registro
+            char opc;
+            Registro producto = new Registro();
+
+            do
+            {
+                //Menu
+                Console.Clear();
+                Console.WriteLine("-Sistema de inventarios Amazon-");
+                Console.WriteLine("Bienvenido, ¿qué es lo que deseas hacer?");
+                Console.WriteLine("1) Registrar productos. \n2) Consultar productos registrados. \n3) Salir del programa.");
+                opc = Console.ReadKey().KeyChar;
+
+                switch (opc)
+                {
+                    case '1':
+                        Console.Clear();
+                        producto.Registrar();
+                        break;
+
+                    case '2':
+                        Console.Clear();
+                        producto.Consultar();
+                        break;
+
+                    case '3':
+                        Console.Clear();
+                        Console.WriteLine("No vemos, GRACIAS por usar nuestra aplicación.");
+                        Console.ReadKey();
+                        break;
+
+                    default:
+                        Console.WriteLine("Esa opción no es valida...");
+                        Console.WriteLine("Presiona cualquier tecla para continuar...");
+                        Console.ReadKey();
+
+                        break;
+                
+                }
+
+            } while (opc != '3');
+
+            
+        }
+    }
+}
---
+++ Examen Unidad 6/Registro.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Examen_Unidad_6
+{
+    //Clase
+    class Registro
+    {
+        //atributos
+        string nombre;
+        string descripcion;
+        float precio;
+        int cant;
+        
+        
+
+        //Metodos
+        public void Registrar()
+        {
+            StreamWriter sw = new StreamWriter("Productos.txt",true);
+            
+            char resp;
+            try
+            {
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("- Bienvendio al registro de productos");
+
+                    Console.WriteLine("- Ahora ingresarás todos los datos necesarios para registrar el nuevo producto.");
+
+                    Console.Write("* Indica el nombre del nuevo producto: ");
+                    nombre = Console.ReadLine();
+
+                    Console.Write("\n* Indica la descripción del nuevo producto: ");
+                    descripcion = Console.ReadLine();
+
+                    Console.Write("\n* Indica el precio del nuevo producto: ");
+                    precio = int.Parse(Console.ReadLine());
+
+                    Console.Write("\n* Indica la cantidad que habrá de deste producto en stock: ");
+                    cant = int.Parse(Console.ReadLine());
+                   
+                    sw.WriteLine("\n*Nombre: " + nombre);
+                    sw.WriteLine("*Descripción: " + descripcion);
+                    sw.WriteLine("*Precio: {0:C}", precio);
+                    sw.WriteLine("*Cantidad en stock: " + cant);
+
+                    Console.WriteLine("Se registró tu producto en el archivo.");
+
+
+
+
+
+                    Console.WriteLine("¿Deseas seguir capturando productos? (s/n)");
+                    resp = Console.ReadKey().KeyChar;
+                } while ((resp == 's') || (resp == 'S'));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+
+                if (sw != null)
+                    sw.Close();
+            }
+
+            Console.WriteLine("\n\nPresiona cualquier tecla para continuar...");
+            Console.ReadKey();
+        }
+
+
+        public void Consultar()
+        {
+            StreamReader sr = new StreamReader("Productos.txt");
+            string registro;
+            try
+            {
+                
+                
+
+                registro = sr.ReadLine();
+
+                Console.WriteLine("Bienvenido al registro de todos los productos almacenados.");
+
+                while (registro != null)
+                {
+                    Console.WriteLine(registro);
+                    registro = sr.ReadLine();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+
+                if (sr != null)
+                    sr.Close();
+            }
+
+            Console.WriteLine("\n\nPresiona cualquier tecla para continuar...");
+            Console.ReadKey();
+        }
+
+
+        ~Registro()
+        {
+            Console.WriteLine("MEMORIA LIBERADA CLASE REGISTRO.");
+        }
+    }
+}
